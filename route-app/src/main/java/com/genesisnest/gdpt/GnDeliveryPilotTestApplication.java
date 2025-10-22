e5b7f3d06f1be4c90e9f4b10bbcf6efc987d09dd
ParkSangJun <parksangjun@parksangjunui-macbookpro.local>
2025-10-22 10:35:35 +0900

    build: 프로젝트 멀티 모듈 초기 세팅

--- route-app/src/main/java/com/genesisnest/gdpt/GnDeliveryPilotTestApplication.java
+++ route-app/src/main/java/com/genesisnest/gdpt/GnDeliveryPilotTestApplication.java
@@ -1,4 +1,11 @@
-package com.genesisnest.gdpt;public class GnDeliveryPilotTestApplication {
+package com.genesisnest.gdpt;
 
-      
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnDeliveryPilotTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GnDeliveryPilotTestApplication.class, args);
+    }
 }
